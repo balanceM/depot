3d12ff30f61ed0b725c189964ea0e36c0b6fef2a
balanceM <286515744@qq.com>
2017-09-14 06:17:48 -0700

    Dash

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,9 +59,16 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    if @line_item.quantity == 1
+      @line_item.destroy
+    elsif @line_item.quantity > 1
+      @line_item.quantity -= 1
+      @line_item.save
+    else
+      throw :abort
+    end
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
