a1d0e533b13f936c78d08ae4a3b3f7ac0dafa287
balanceM <286515744@qq.com>
2017-09-14 20:24:32 -0700

    Check out

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, optional: true
   belongs_to :cart
-
+  belongs_to :order, optional: true
   def total_price
     product.price*quantity
   end
